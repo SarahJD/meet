ff419e99b438273423d9787560b07004dcf7620a
SarahJD <sarahjdudek@gmail.com>
2021-03-12 16:11:29 +0100

    comment out functions for verification of app

--- src/api.js
+++ src/api.js
@@ -1,30 +1,54 @@
-const checkToken = async (accessToken) => {
-  const result = await fetch (
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-  )
-    .then((res) => res.json())
-    .catch((error) => error.json());
+// const checkToken = async (accessToken) => {
+//   const result = await fetch (
+//     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+//   )
+//     .then((res) => res.json())
+//     .catch((error) => error.json());
 
-    return result.error ? false : true;
-};
+//     return tokenCheck === false ? (
+//       <div className="App">
+//       <Login />
+//       </div>
+//       ) : (return (result.error ? false : true);
+//       </div>);
+// };
 
-const getAccessToken = async () => {
-  const accessToken = await localStorage.getItem('access_token');
-  const tokenCheck = accessToken && (await checkToken(accessToken));
+// const removeQuery = () => {
+//   if (window.history.pushState &&
+//   window.location.pathname) {
+//   var newurl =
+//   window.location.protocol +
+//   "//" +
+//   window.location.host +
+//   window.location.pathname;
+//   window.history.pushState("", "", newurl);
+//   } else {
+//   newurl = window.location.protocol + "//" +
+//   window.location.host;
+//   window.history.pushState("", "", newurl);
+//   }
+//   };
 
-  if (!accessToken || !tokenCheck) {
-    await localStorage.removeItem('access_token');
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get('code');
-    if (!code) {
-      const results = await axios.get('YOUR API getAuth ENDPOINT HERE');
-      const { authUrl } = results.data;
-      return (window.location.href = authUrl)
-    }
-    return code && getToken(code);
-  }
-  return accessToken;
-};
+// const getAccessToken = async () => {
+
+//   const getToken = async (code) => {
+//     const getToken = async (code) => {
+//       removeQuery();
+//       const encodeCode = encodeURIComponent(code);
+//       const { access_token } = await fetch(
+//       `https://f1k17pnw2a.execute-api.us-east-1.amazonaws.c
+//       om/dev/api/token/${encodeCode}`
+//       )
+//       .then((res) => {
+//       return res.json();
+//       })
+//       .catch((error) => error);
+//       Page 6
+//       access_token &&
+//       localStorage.setItem("access_token", access_token);
+//       return access_token;
+//       };
+// };
 
 
-export default checkToken;
+// export default checkToken; 
