97a8e1dcf6cd07329037e9ca2846213699015d8f
SarahJD <sarahjdudek@gmail.com>
2021-03-15 12:25:23 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <EventList />
+        <CitySearch />
+        <NumberOfEvents />
       </div>
     );
   }
