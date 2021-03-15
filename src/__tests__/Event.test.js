0dc0d88c415aa05f60ba21b07c20e755c6227c85
SarahJD <sarahjdudek@gmail.com>
2021-03-15 12:31:47 +0100

    create test suite for Event component

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Event from '../Event';
+import { mockData } from '../mock-data';
+
+describe('<Event /> component', () => {
+  let EventWrapper;
+  beforeAll(() => {
+    EventWrapper = shallow(<Event />);
+  });
+  test('render title/summary of each event', () => {
+    expect(EventWrapper.find('.summary')).toHaveLength(1);
+  });
+  test('render start time of each event', () => {
+    expect(EventWrapper.find('.start')).toHaveLength(1);
+  });
+  test('render @summary of each event', () => {
+    expect(EventWrapper.find('.atsummary')).toHaveLength(1);
+  });
+  test('render location of each event', () => {
+    expect(EventWrapper.find('.location')).toHaveLength(1);
+  });
+  test('render show-details-button of each event', () => {
+    expect(EventWrapper.find('.btn-details')).toHaveLength(1);
+  });
+  test('render details element', () => {
+    expect(EventWrapper.find('.details')).toHaveLength(1);
+  });
+  test('render about-event-header', () => {
+    expect(EventWrapper.find('.about-header')).toHaveLength(1);
+  });
+  test('render link to google-calendar', () => {
+    expect(EventWrapper.find('.calendar-link')).toHaveLength(1);
+  });
+  test('render description for each event', () => {
+    expect(EventWrapper.find('.description')).toHaveLength(1);
+  });
+  test('render hide-details-button of each event', () => {
+    expect(EventWrapper.find('.btn-hide-details')).toHaveLength(1);
+  });
+  test('click on show-details-button expands event details', () => {
+    EventWrapper.setState({ showDetails: false});
+    EventWrapper.find('.btn-details').simulate('click');
+    expect(EventWrapper.state('showDetails')).toBe(true);
+  });
+  test('click on hide-details-button collapses event details', () => {
+    EventWrapper.find('.btn-hide-details').simulate('click');
+    expect(EventWrapper.state('showDetails')).toBe(false);
+  });
+});
+
