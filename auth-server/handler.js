7a23fc3a0a7a0e72d75bc76e434db8630bd6b315
SarahJD <sarahjdudek@gmail.com>
2021-03-17 14:43:19 +0100

    delete comma in credentials object

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -19,7 +19,7 @@ const credentials = {
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   redirect_uris: ["https://sarahjd.github.io/meet/"],
-  javascript_origins: ["https://SarahJD.github.io", "http://localhost:8080"],
+  javascript_origins: ["https://SarahJD.github.io", "http://localhost:8080"]
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
