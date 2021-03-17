7f94d8290107b414098c9ed881805bd4a4868cc3
SarahJD <sarahjdudek@gmail.com>
2021-03-17 14:46:38 +0100

    update component with mock-data

--- src/Event.js
+++ src/Event.js
@@ -18,21 +18,34 @@ class Event extends Component {
   };
 
   render() {
+    const { event } = this.props;
+    const showDetails = this.state.showDetails;
+
+
     return (
       <div className="Event">
         <section className="overview">
-          <h2 className="summary"></h2>
-          <p className="start"></p>
-          <p className="atsummary"></p>
-          <p className="location"></p>
-          <button className="btn-details" onClick={this.handleShowDetails}></button>
+          <h2 className="summary">{event.summary}</h2>
+          {/* {<div className="start-time">event.start.map((info) => (
+            <p key={info}>{info}</p>))} */}
+          <p className="start-time">{event.start.dateTime}</p>
+          <p className="time-zone">(time zone: {event.start.timeZone})</p>
+          <p className="location">{event.location}</p>
+        {!showDetails && (
+          <button className="btn-details" onClick={this.handleShowDetails}>show details</button>
+        )}
         </section>
+
+        {showDetails && (
         <section className="details"> 
-          <h3 className="about-header"></h3>
-          <p className="htmlLink"></p>
-          <p className="description"></p>
-          <button className="btn-hide-details" onClick={this.handleShowDetails}></button>
+          <h3 className="about-header">About event:</h3>
+          <a className="htmlLink" href="{event.htmlLink}">{event.htmlLink}</a>
+          <p className="end-time">{event.end.dateTime}</p>
+          <p className="description">{event.description}</p>
+          <p className="organizer-email">{event.organizer.email}</p>      
+          <button className="btn-hide-details" onClick={this.handleShowDetails}>hide details</button>
         </section>
+        )}
       </div>
     );
   }
