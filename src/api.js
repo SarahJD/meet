4bf80ded160d5e99340f74285ecc5db601738ea7
SarahJD <sarahjdudek@gmail.com>
2021-03-19 15:52:25 +0100

    create functions for integrating Google Calendar API

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import { mockData } from './mock-data';
+import axios from 'axios';
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
@@ -6,63 +7,84 @@ export const extractLocations = (events) => {
   return locations;
 };
 
-export const getEvents = async () => {
-  return mockData;
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
+  
+  return result;
 };
 
+export const getEvents = async () => {
+  NProgress.start();
 
-// FOR VALIDATION
-// const checkToken = async (accessToken) => {
-//   const result = await fetch (
-//     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-//   )
-//     .then((res) => res.json())
-//     .catch((error) => error.json());
+  if(window.location.href.startsWith('http://localhost')) {
+    return mockData;
+  }
 
-//     return tokenCheck === false ? (
-//       <div className="App">
-//       <Login />
-//       </div>
-//       ) : (return (result.error ? false : true);
-//       </div>);
-// };
+  const token = await getAccessToken();
 
-// const removeQuery = () => {
-//   if (window.history.pushState &&
-//   window.location.pathname) {
-//   var newurl =
-//   window.location.protocol +
-//   "//" +
-//   window.location.host +
-//   window.location.pathname;
-//   window.history.pushState("", "", newurl);
-//   } else {
-//   newurl = window.location.protocol + "//" +
-//   window.location.host;
-//   window.history.pushState("", "", newurl);
-//   }
-//   };
+  if(token) {
+    removeQuery();
+    const url = 'https://1ho8gdafy6.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
+    const result = await axios.get(url);
+    if (result.data) {
+      var locations = extractLocations(result.data.events);
+      localStorage.setItem('lastEvents', JSON.stringify(result.data));
+      localStorage.setItem('locations', JSON.stringify(locations));
+    }
+    NProgress.done();
+    return result.data.events;
+  }
+};
 
-// const getAccessToken = async () => {
+export const getAccessToken = async() => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
 
-//   const getToken = async (code) => {
-//     const getToken = async (code) => {
-//       removeQuery();
-//       const encodeCode = encodeURIComponent(code);
-//       const { access_token } = await fetch(
-//       `https://f1k17pnw2a.execute-api.us-east-1.amazonaws.c
-//       om/dev/api/token/${encodeCode}`
-//       )
-//       .then((res) => {
-//       return res.json();
-//       })
-//       .catch((error) => error);
-//       Page 6
-//       access_token &&
-//       localStorage.setItem("access_token", access_token);
-//       return access_token;
-//       };
-// };
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem('access_token');
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get('code');
+    if (!code) {
+      const results = await axios.get(
+        'https://1ho8gdafy6.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url'
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+}
 
+const removeQuery = () => {
+  if(window.history.pushState && window.location.pathname) {
+    var newurl = 
+      window.location.protocol +
+      '//' +
+      window.location.host + 
+      window.location.pathname;
+    window.history.pushState('', '', newurl);
+  } else {
+    newurl = window.location.protocol + '//' + window.location.host;
+    window.history.pushState('', '', newurl);
+  }
+};
 
-// export default checkToken; 
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    'https://1ho8gdafy6.execute-api.eu-central-1.amazonaws.com/dev/api/token' +'/' + encodeCode
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => error);
+
+  access_token && localStorage.setItem('access_token', access_token);
+
+  return access_token;
+};
